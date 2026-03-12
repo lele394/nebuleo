b8e12a8e33cc052a7f04e0111abbbf743317a64b
lele394 <lele394prod@gmail.com>
2026-03-12 19:31:22 +0100

    Update astro.config.mjs with new configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import rehypeKatex from 'rehype-katex';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://lele394.github.io',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		remarkPlugins: [remarkMath],
